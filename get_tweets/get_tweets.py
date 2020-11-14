5b5e7037d8c6077ada7ebe5261c7d5bc812f1e97
eric <eric.j.blom@gmail.com>
2020-11-14 14:06:35 -0800

    getting lots of tweets successfully

--- get_tweets/get_tweets.py
+++ get_tweets/get_tweets.py
@@ -3,6 +3,7 @@ import tweepy
 import pandas as pd
 from json import load
 from os import path
+import datetime
 
 root = '..'
 fn = 'secret.json'
@@ -15,6 +16,8 @@ auth = tweepy.OAuthHandler(secrets['ApiKey'], secrets['ApiKeySecret'])
 auth.set_access_token(secrets['AccessToken'], secrets['AccessTokenSecret'])
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+week_ago = datetime.datetime.now() - datetime.timedelta(days=28)
 
 tweets = []
 
@@ -50,30 +53,41 @@ params = { 'screen_name':userID,
            'tweet_mode' : 'extended'
 }
 data = []
-
+reached_date = False
 while True:
     if len(data):
-        params['max_id'] = data[-1]['id'] - 1
+        params['max_id'] = data[-1][1] # - 1
     tweets = api.user_timeline(**params)
     if len(tweets) == 0: # got to end of tweets
         break
 
     for tweet in tweets:
+        if tweet.created_at <= week_ago:
+            reached_date = True
+            break
+
+        # simplify hashtags
+        hashtags = []
+        for ht in tweet.entities['hashtags']:
+            hashtags.append(ht['text'])
+        hashtags = ','.join(hashtags)
         onetweet = [tweet.created_at,
                     tweet.id,
                     tweet.full_text,
                     tweet.user._json['screen_name'],
-                    tweet.entities['hashtags']
+                    hashtags
                     ]
-    data.append(tuple(onetweet))
-
-tweets = api.user_timeline(screen_name=userID,
-                           # 200 is the maximum allowed count
-                           count=200,
-                           include_rts = False,
-                           # Necessary to keep full_text
-                           # otherwise only the first 140 words are extracted
-                           tweet_mode = 'extended'
-                           )
+        data.append(tuple(onetweet))
+    if reached_date:
+        break
+# tweets = api.user_timeline(screen_name=userID,
+#                            # 200 is the maximum allowed count
+#                            count=200,
+#                            include_rts = False,
+#                            # Necessary to keep full_text
+#                            # otherwise only the first 140 words are extracted
+#                            tweet_mode = 'extended'
+#                            )
 
 df = pd.DataFrame(data, columns = ['created_at','tweet_id', 'tweet_text', 'screen_name', 'hashtag',])
+df.to_csv('test_tweets.csv')
